e55395e6d527679ee37fe19a44773d488f6c6aaf
Daan Hautekeete <hautekeete.daan@gmail.com>
2024-05-22 15:28:57 +0200

    Maken form menu
    - Design van form menu gemaakt
    
    - Code toegevoegd zodat knoppen van kleur veranderen indien muis boven de knoppen zijn

---
+++ prjSofoTaakKlassesEnObjecten/prjSofoTaakKlassesEnObjecten/frmMenu.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace prjSofoTaakKlassesEnObjecten
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbContactpersonen = new System.Windows.Forms.ListBox();
+            this.lsbBasisinfoContactpersonen = new System.Windows.Forms.ListBox();
+            this.btnNieuwContactpersoon = new System.Windows.Forms.Button();
+            this.btnWijzigContactpersoon = new System.Windows.Forms.Button();
+            this.btnVerwijderContactpersoon = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsbContactpersonen
+            // 
+            this.lsbContactpersonen.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lsbContactpersonen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lsbContactpersonen.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbContactpersonen.FormattingEnabled = true;
+            this.lsbContactpersonen.ItemHeight = 14;
+            this.lsbContactpersonen.Location = new System.Drawing.Point(13, 17);
+            this.lsbContactpersonen.Name = "lsbContactpersonen";
+            this.lsbContactpersonen.Size = new System.Drawing.Size(229, 282);
+            this.lsbContactpersonen.TabIndex = 0;
+            // 
+            // lsbBasisinfoContactpersonen
+            // 
+            this.lsbBasisinfoContactpersonen.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lsbBasisinfoContactpersonen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lsbBasisinfoContactpersonen.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbBasisinfoContactpersonen.FormattingEnabled = true;
+            this.lsbBasisinfoContactpersonen.ItemHeight = 14;
+            this.lsbBasisinfoContactpersonen.Location = new System.Drawing.Point(13, 309);
+            this.lsbBasisinfoContactpersonen.Name = "lsbBasisinfoContactpersonen";
+            this.lsbBasisinfoContactpersonen.Size = new System.Drawing.Size(561, 170);
+            this.lsbBasisinfoContactpersonen.TabIndex = 1;
+            // 
+            // btnNieuwContactpersoon
+            // 
+            this.btnNieuwContactpersoon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNieuwContactpersoon.Font = new System.Drawing.Font("Mongolian Baiti", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNieuwContactpersoon.Location = new System.Drawing.Point(259, 17);
+            this.btnNieuwContactpersoon.Name = "btnNieuwContactpersoon";
+            this.btnNieuwContactpersoon.Size = new System.Drawing.Size(315, 60);
+            this.btnNieuwContactpersoon.TabIndex = 2;
+            this.btnNieuwContactpersoon.Text = "Nieuw";
+            this.btnNieuwContactpersoon.UseVisualStyleBackColor = true;
+            // 
+            // btnWijzigContactpersoon
+            // 
+            this.btnWijzigContactpersoon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWijzigContactpersoon.Font = new System.Drawing.Font("Mongolian Baiti", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWijzigContactpersoon.Location = new System.Drawing.Point(259, 87);
+            this.btnWijzigContactpersoon.Name = "btnWijzigContactpersoon";
+            this.btnWijzigContactpersoon.Size = new System.Drawing.Size(315, 60);
+            this.btnWijzigContactpersoon.TabIndex = 3;
+            this.btnWijzigContactpersoon.Text = "Wijzig";
+            this.btnWijzigContactpersoon.UseVisualStyleBackColor = true;
+            // 
+            // btnVerwijderContactpersoon
+            // 
+            this.btnVerwijderContactpersoon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerwijderContactpersoon.Font = new System.Drawing.Font("Mongolian Baiti", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerwijderContactpersoon.Location = new System.Drawing.Point(259, 161);
+            this.btnVerwijderContactpersoon.Name = "btnVerwijderContactpersoon";
+            this.btnVerwijderContactpersoon.Size = new System.Drawing.Size(315, 60);
+            this.btnVerwijderContactpersoon.TabIndex = 4;
+            this.btnVerwijderContactpersoon.Text = "Verwijder";
+            this.btnVerwijderContactpersoon.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Mongolian Baiti", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.Location = new System.Drawing.Point(259, 235);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(315, 60);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(586, 493);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnVerwijderContactpersoon);
+            this.Controls.Add(this.btnWijzigContactpersoon);
+            this.Controls.Add(this.btnNieuwContactpersoon);
+            this.Controls.Add(this.lsbBasisinfoContactpersonen);
+            this.Controls.Add(this.lsbContactpersonen);
+            this.Name = "frmMenu";
+            this.Text = "Contact Manager - Homescreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbContactpersonen;
+        private System.Windows.Forms.ListBox lsbBasisinfoContactpersonen;
+        private System.Windows.Forms.Button btnNieuwContactpersoon;
+        private System.Windows.Forms.Button btnWijzigContactpersoon;
+        private System.Windows.Forms.Button btnVerwijderContactpersoon;
+        private System.Windows.Forms.Button btnSettings;
+    }
+}
+
---
+++ prjSofoTaakKlassesEnObjecten/prjSofoTaakKlassesEnObjecten/frmMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjSofoTaakKlassesEnObjecten
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+
+            //code om knoppen te laten veranderen van kleur bij hover
+            btnNieuwContactpersoon.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnWijzigContactpersoon.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnVerwijderContactpersoon.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnSettings.FlatAppearance.MouseOverBackColor = Color.Gray;
+        }
+    }
+}
